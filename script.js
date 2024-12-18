7ea3f1a45d2995ee65bd454412c70e9a61be3874
PreetsCh <pritabytyqi@gmail.com>
2024-12-18 16:48:56 +0100

    Added API

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+function searchCity(city){
+ 
+    let apiKey = "8fbf7bbacd67ca6b5t4o3f620d474a76";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units="metric"`;
+
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value)
 }
 
 
