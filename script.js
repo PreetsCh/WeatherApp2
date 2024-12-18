a83a00d1c5b1ff84974e05f8c500ba84c8fb4f9e
PreetsCh <pritabytyqi@gmail.com>
2024-12-18 19:16:15 +0100

    Api works!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function searchCity(city){
  
     let apiKey = "8fbf7bbacd67ca6b5t4o3f620d474a76";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units="metric"`;
-
+console.log(apiUrl);
 }
 
 
