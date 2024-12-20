bff4090b67cf6cb87566bfda762f32494896f9cd
PreetsCh <pritabytyqi@gmail.com>
2024-12-20 18:01:58 +0100

    Updated the forecast with current data in js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
+    let iconElement = document.querySelector("#icon");
 
     console.log(response.data);
 
@@ -16,7 +16,7 @@ let iconElement = document.querySelector("#icon");
      descriptionElement.innerHTML=response.data.condition.description;
      temperatureElement.innerHTML=Math.round(temperature);
      humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
-     windSpeedElement.innerHTML=`${response.data.wind.speed} km/h`;
+     windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
      iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     
      getForecast(response.data.city);
@@ -46,8 +46,9 @@ function formatDate(date) {
 function searchCity(city){
     let apiKey = "8fbf7bbacd67ca6b5t4o3f620d474a76";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-   console.log(apiUrl);
-    axios.get(apiUrl).then(refreshWeather);
+ 
+    axios.get(apiUrl).then(refreshWeather)
+      .catch((error) => console.error("Error fetching weather data:", error));
 }
 
 
@@ -57,34 +58,44 @@ function handleSearchSubmit(event) {
   
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "8fbf7bbacd67ca6b5t4o3f620d474a76";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
-  axios(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+    let forecastHtml = "";
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = "";
-
-  days.forEach(function (day) {
+   response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </div>
       </div>
     `;
+          }
   });
 
   let forecastElement = document.querySelector("#forecast");
